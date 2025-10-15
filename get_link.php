c822be82ca2e465311317f1c31242dcef0505fec
Pepel <101939048+yologet1@users.noreply.github.com>
2025-10-16 02:51:50 +0300

    Delete get_link.php

--- get_link.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-$id = $_GET['to'] ?? '';
-$f = __DIR__ . '/push_' . $id . '.txt';
-if (!file_exists($f)) { echo ''; exit; }
-echo file_get_contents($f);
-unlink($f);
