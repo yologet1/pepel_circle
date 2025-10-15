2d5879c018fbc995b146dab3cafcb94974daea6d
Pepel <101939048+yologet1@users.noreply.github.com>
2025-10-16 02:52:49 +0300

    Delete push_link.php

--- push_link.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-$data = json_decode(file_get_contents("php://input"), true);
-if (!$data['to'] || !$data['link']) die('ERR');
-$f = __DIR__ . '/push_' . $data['to'] . '.txt';
-file_put_contents($f, $data['link']);
-echo 'OK';
